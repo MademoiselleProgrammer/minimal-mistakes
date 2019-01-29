f7364c3e2759a26cc2c992771a46012979a13804
Roxa <meowcollins@gmail.com>
2019-01-29 08:19:04 -0700

    Test for seeing getting example webpage to work
    used bundle exec jekyll serve --watch to view locally on computer

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,41 @@
+var store = [{
+        "title": "Welcome",
+        "excerpt":"Welcome to the to the CHORDS Portal User Site, your guide to all things CHORDS. Here you will find tutorials and reference materials. The occaisional blog entry will announce new features, provides tips and answer questions. If you have any questions, feel free to drop a line to the CHORDS...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/welcome/",
+        "teaser":null},{
+        "title": "Responsive Design",
+        "excerpt":"The CHORDS Portal has been reworked to use “responsive design”. This means that the web site will display nicely on all sizes of devices, from phones to desktops.   This is made possible using the awesome Twitter boostrap styling toolkit.  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/boostrap/",
+        "teaser":null},{
+        "title": "Open Source CHORDS Portal for the Geosciences Video",
+        "excerpt":"Mike Daniels and Charlie Martin presenting the CHORDS portal at NCAR - Open Source CHORDS Portal for the Geosciences.  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/chords-video/",
+        "teaser":null},{
+        "title": "CHORDS is being adapted to run under Docker",
+        "excerpt":"We are working on making a CHORDS portal available using Docker. This means that you will be able to run a CHORDS portal locally, on your own Mac, Windows or Linux system. We will also upgrade the CHORDS cloud creation processs so that the Docker images are used on AWS...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/docker-development/",
+        "teaser":null},{
+        "title": "Docker Advances",
+        "excerpt":"The Docker version of CHORDS now has a persistent database. This means that you can start and stop your docker engine, your CHORDS docker containers, or restart the server, and the database will survive. It’s also very easy to update to new releases of CHORDS without loosing any data. Instructions...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/docker-advances/",
+        "teaser":null},{
+        "title": "CHORDS Switches To InfluxDB",
+        "excerpt":"CHORDS has adopted the InfluxDB time-series database, from influxdata. InfluxDB is a high performance database, designed specifically for time-series appllications. Changing from the original MySQL database for data storage allows CHORDS to manage, navigate, and deliver much larger datasets. Currently active CHORDS deployments have reached the size of 50M measurements....","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/influxdb/",
+        "teaser":null},{
+        "title": "Grafana Visualization Added",
+        "excerpt":"Powerful dashboard capabilities have been added to CHORDS. Users can create sophisticated realtime displays, with exceptional time navigation features, right in their browser. These are provided via the terrific opensource Grafana system. Grafana is implemented in CHORDS as a separate web server running as a Docker container. Highly responsive graphics...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/grafana/",
+        "teaser":null},{
+        "title": "New EarthCube blog article featuring CHORDS",
+        "excerpt":"A new EarthCube blog article entitled “Striking Power CHORDS with the Geosciences” has been posted to the EarthCube blog site.   Read about the current status of the project and the planned developments for the future.  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/chords-earthcube-blog/",
+        "teaser":null}]
